8387da13aeb69361968cc3f4ee264d0ad86fdb36
maryembaldi <maryem.baldi@esprit.tn>
2024-03-01 11:26:46 +0100

    first commit

---
+++ src/main/java/com/example/maryembaldiexamen/MaryemBaldiExamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.maryembaldiexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaryemBaldiExamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaryemBaldiExamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maryembaldiexamen/MaryemBaldiExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maryembaldiexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaryemBaldiExamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
